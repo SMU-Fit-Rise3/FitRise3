2881471be5e6a542b0a6eb2044c502d044d7e551
woojin-choi518 <woojin.choi518@gmail.com>
2024-03-18 10:52:51 +0900

    feat: Login UI implementation #2

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+import images from "./images";
+
+export {images};
+//여기에 다 export 해놔서 경로 짧게 쓸 수 있었음.
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import  CustomBtn from "./CustomBtn"
+
+//import TabBar from "./TabBar";
+//import Character from "./Character";
+
+export{
+    CustomBtn
+}
+
+//인덱스 파일을 통해 간단히 import, export 할 수 있게!!!
